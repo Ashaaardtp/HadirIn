4e611e4defe6d178907928312b82446b6e575cac
Pasha Raditya Putra <ungu55.pr@gmail.com>
2026-04-09 12:52:35 +0700

    Update Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,12 +25,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${poppins.variable} ${playfair.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col font-sans bg-midnight-dark">
-        <main className="flex-1">{children}</main>
-      </body>
-    </html>
+    <>
+      <Analytics />
+      <html
+        lang="en"
+        className={`${poppins.variable} ${playfair.variable} h-full antialiased`}>
+        <body className="min-h-full flex flex-col font-sans bg-midnight-dark">
+          <main className="flex-1">
+            {children}
+          </main>
+        </body>
+      </html>
+    </>
   );
 }
